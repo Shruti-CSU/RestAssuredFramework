e3fbce869afa816c7ef55b94df14033915f0359d
Shruti-CSU <shrutipatel715@gmail.com>
2026-02-19 12:44:12 -0600

    Add import for RestAssured in CreatePostTest

--- src/test/java/com/api/tests/CreatePostTest.java
+++ src/test/java/com/api/tests/CreatePostTest.java
@@ -8,6 +8,7 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+
 import static io.restassured.RestAssured.given;
 
 public class CreatePostTest extends BaseTest {
